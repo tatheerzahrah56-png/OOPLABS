3377334b44b01265ce47da7967f27a867a9264db
tatheerzahrah56-png <tatheerzahrah56@gmail.com>
2026-06-09 01:38:47 -0700

    Add files via upload

---
+++ lab 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void display() {
+        cout << name << " " << age << endl;
+    }
+};
+
+int main() {
+    Student s1;
+    s1.name = "Ali";
+    s1.age = 20;
+
+    s1.display();
+    return 0;
+}
