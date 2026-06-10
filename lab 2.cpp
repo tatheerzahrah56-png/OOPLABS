f8c8a6ed48baf7acfca9c6f29dc66e8c27b63b2a
tatheerzahrah56-png <tatheerzahrah56@gmail.com>
2026-06-09 23:41:05 -0700

    Add files via upload

---
+++ lab 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() = 0; // pure virtual function
+};
+
+class Circle : public Shape {
+public:
+    void draw() {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+int main() {
+    Shape* s;
+    Circle c;
+
+    s = &c;
+    s->draw();
+
+    return 0;
+}
