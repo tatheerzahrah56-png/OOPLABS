cb948aa6359ce4feafcee2885766e51a4d69d86c
tatheerzahrah56-png <tatheerzahrah56@gmail.com>
2026-06-09 23:45:30 -0700

    Add files via upload

---
+++ lab 6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void sound() {
+        cout << "Animal makes sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Dog d;
+    d.sound();
+    d.bark();
+
+    return 0;
+}
